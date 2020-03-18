3ce955631751f814cd98093d22563dd9a4495885
laureanray <laureanraybahala@gmail.com>
2020-03-18 21:09:04 +0800

    added bootstrap

--- src/app/modules/app/app.module.ts
+++ src/app/modules/app/app.module.ts
@@ -7,18 +7,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from '../../shared/home/home.component';
 import {StudentLoginComponent} from '../../shared/student-login/student-login.component';
 import {InstructorAdminLoginComponent} from '../../shared/instructor-admin-login/instructor-admin-login.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     StudentLoginComponent,
-    InstructorAdminLoginComponent
+    InstructorAdminLoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

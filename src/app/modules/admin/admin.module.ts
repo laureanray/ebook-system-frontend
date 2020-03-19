27932584a263a59f4a65b28871f257dc8f648269
alelelene <cmdgesteban@gmail.com>
2020-03-19 21:35:10 +0800

    added book card

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,14 +3,23 @@ import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin/admin.component';
 import {AdminRoutingModule} from "./admin-routing.module";
 import { BookManagerComponent } from './book-manager/book-manager.component';
+import {SidebarNavComponent} from "./sidebar-nav/sidebar-nav.component";
+import {MatListModule} from "@angular/material/list";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
 
 
 
 @NgModule({
-  declarations: [AdminComponent, BookManagerComponent],
+  declarations: [AdminComponent, BookManagerComponent, SidebarNavComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressBarModule
   ]
 })
 export class AdminModule { }

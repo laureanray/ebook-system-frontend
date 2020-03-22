906c4b939a8c82981248f13ca36eb64c8f3ed862
alelelene <cmdgesteban@gmail.com>
2020-03-23 00:21:20 +0800

    added faculty add user form

--- src/app/modules/admin/components/add-user/add-user.component.ts
+++ src/app/modules/admin/components/add-user/add-user.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-user.component.sass']
 })
 export class AddUserComponent implements OnInit {
-
+  public showStudent = true;
+  public showFaculty = false;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  toggleStudent() {
+    this.showStudent = true;
+    this.showFaculty = false;
+  }
+  toggleFaculty() {
+    this.showStudent = false;
+    this.showFaculty = true;
+  }
 }

7c5255b9ba67a9e390ced574b02825ba94366d16
alelelene <cmdgesteban@gmail.com>
2020-03-27 00:33:26 +0800

    added book library

---
+++ src/app/modules/student/components/book-library/book-library.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-library',
+  templateUrl: './book-library.component.html',
+  styleUrls: ['./book-library.component.sass']
+})
+export class BookLibraryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/student/student-routing-module.ts
+++ src/app/modules/student/student-routing-module.ts
@@ -4,12 +4,17 @@ import {StudentComponent} from './components/student/student.component';
 import {NgModule} from '@angular/core';
 import {SetPasswordComponent} from './components/set-password/set-password.component';
 import {HomeComponent} from './components/home/home.component';
+import {BookLibraryComponent} from './components/book-library/book-library.component';
+import {InitialHomeComponent} from "./components/initial-home/initial-home.component";
+// import {InitialHomeComponent} from './components/initial-home/initial-home.component';
 
 const routes: Routes = [
   { path: 'set-password', component: SetPasswordComponent},
   { path: '', component: StudentComponent,
     children: [
-      { path: 'home', component: HomeComponent}
+      { path: 'home1', component: InitialHomeComponent},
+      { path: 'home', component: HomeComponent},
+      { path: 'book-library', component: BookLibraryComponent}
     ]
     }
 ];

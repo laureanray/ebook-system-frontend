1c3985d25bc8b4d787e95f3c2ded1c21b0bca984
laureanray <laureanraybahala@gmail.com>
2020-04-06 00:37:16 +0800

    refactor

--- src/app/modules/instructor/instructor.module.ts
+++ src/app/modules/instructor/instructor.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InstructorComponent } from './instructor/instructor.component';
 import {InstructorRoutingModule} from './instructor-routing.module';
-import { InstructorSidebarComponent } from './instructor-sidebar/instructor-sidebar.component';
-import {MatListModule} from "@angular/material/list";
+// import { InstructorSidebarComponent } from './instructor-sidebar/instructor-sidebar.component';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
-  declarations: [InstructorComponent, InstructorSidebarComponent],
+  declarations: [InstructorComponent],
     imports: [
         CommonModule,
         InstructorRoutingModule,
